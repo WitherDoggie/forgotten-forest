6e583c3179830201ed98d2d98badb3a5c69192a6
Wither <elabont@siue.edu>
2021-03-11 09:09:48 -0600

    Optimizations

--- README.md
+++ README.md
@@ -5,3 +5,22 @@ Enter the forgotten lands and face the new dangers and adventures trapped within
 ## License
 
 Available under the LGPL-3.0 license.
+
+##F.A.Q
+- Will there be a Forge version?
+   - I will not be developing a Forge version of this mod, however,   
+     you are free to convert the mod to Forge yourself.  
+     
+       
+- When is the next update?
+    - Updates are released when I feel the new content is stable and polished enough for release.  
+    
+    
+- Can you add X feature?
+    - Suggestions are always welcome. To make a suggestion, open an issue on the github page.  
+      This makes it easy to track all open suggestions.  
+        
+
+- Can I include this in modpacks?
+    - Yes. Linking to this page is not required, but is always appreciated.  
+    
--- src/main/java/io/github/witherdoggie/forgottenforest/item/ShadowAmuletFullItem.java
+++ src/main/java/io/github/witherdoggie/forgottenforest/item/ShadowAmuletFullItem.java
@@ -1,15 +1,24 @@
 package io.github.witherdoggie.forgottenforest.item;
 
+import io.github.witherdoggie.forgottenforest.mixin.EntityAccessor;
+import net.fabricmc.fabric.api.dimension.v1.FabricDimensions;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.TeleportTarget;
 import net.minecraft.world.World;
+import net.minecraft.world.dimension.DimensionOptions;
 
 import java.util.List;
 
@@ -26,16 +35,25 @@ public class ShadowAmuletFullItem extends Item {
 
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-        if (this.isFood()) {
-            ItemStack itemStack = user.getStackInHand(hand);
-            if (user.canConsume(this.getFoodComponent().isAlwaysEdible())) {
-                user.setCurrentHand(hand);
-                return TypedActionResult.consume(itemStack);
-            } else {
-                return TypedActionResult.fail(itemStack);
+
+        ItemStack itemStack = user.getStackInHand(hand);
+
+        if(!world.isClient()) {
+            ServerWorld serverWorld = (ServerWorld)world;
+            MinecraftServer minecraftServer = serverWorld.getServer();
+            RegistryKey<World> registryKey = World.NETHER;
+            ServerWorld serverWorld2 = minecraftServer.getWorld(registryKey);
+            //TeleportTarget target = ((EntityAccessor) user).invokeGetTeleportTarget(serverWorld2);
+           // FabricDimensions.teleport(user, serverWorld2, target);
+
+            if (serverWorld2 != null) {
+                user.moveToWorld(serverWorld2);
             }
-        } else {
-            return TypedActionResult.pass(user.getStackInHand(hand));
+            //user.moveToWorld(serverWorld2);
+
+            return TypedActionResult.success(itemStack);
         }
+
+        return TypedActionResult.pass(itemStack);
     }
 }
