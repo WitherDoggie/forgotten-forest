7a761313e0506bf7d60b063eaaef2ff8ad5149a5
Wither <squeakypig34@gmail.com>
2021-08-05 17:42:31 -0500

    World gen is fully ported to 1.17. It still needs adjustments. Mod can be built and is playable.

--- src/main/java/io/github/witherdoggie/forgottenforest/world/surface/SurfaceBuilders.java
+++ src/main/java/io/github/witherdoggie/forgottenforest/world/surface/SurfaceBuilders.java
@@ -9,6 +9,7 @@ public class SurfaceBuilders {
 
     public static final SurfaceBuilder<QuadarySurfaceConfig> GLOOMY_SURFACE_BUILDER = Registry.register(Registry.SURFACE_BUILDER, new Identifier(ForgottenForest.MODID, "gloomy_surface_builder"), new ModBiomeSurfaceBuilder());
     public static final SurfaceBuilder<QuadarySurfaceConfig> FIRE_SURFACE_BUILDER = Registry.register(Registry.SURFACE_BUILDER, new Identifier(ForgottenForest.MODID, "fire_surface_builder"), new ModBiomeSurfaceBuilder());
+    public static final SurfaceBuilder<QuadarySurfaceConfig> SHADOW_SURFACE_BUILDER = Registry.register(Registry.SURFACE_BUILDER, new Identifier(ForgottenForest.MODID, "shadow_surface_builder"), new ModBiomeSurfaceBuilder());
     public static final SurfaceBuilder<QuinteroSurfaceConfig> FIRE_PITS_SURFACE_BUILDER = Registry.register(Registry.SURFACE_BUILDER, new Identifier(ForgottenForest.MODID, "fire_pits_surface_builder"), new ModBiomeSurfaceBuilderFluid());
 
     public static void initSurfaceBuilders(){
